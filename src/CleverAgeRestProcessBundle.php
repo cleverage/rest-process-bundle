f67c1f2a2f1cd9428916f15b7a94c1631f9fd699
Nicolas Joubert <njoubert@clever-age.com>
2024-11-26 14:31:17 +0100

    Apply quality rules

--- src/CleverAgeRestProcessBundle.php
+++ src/CleverAgeRestProcessBundle.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace CleverAge\RestProcessBundle;
 
 use CleverAge\ProcessBundle\DependencyInjection\Compiler\RegistryCompilerPass;
-use CleverAge\RestProcessBundle\Registry\ClientRegistry;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
